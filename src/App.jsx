229a374dec5a010a34b2abeaf3f773e8825822f6
sagarbangade <sagarbangade55@gmail.com>
2025-01-20 13:18:16 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import EmergencyChatBot from "./components/EmergencyChatBot";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <EmergencyChatBot />
+    </div>
+  );
+}
+
+export default App;
